8ca73ab77e6c8675c27166a495d82751bac23c74
Csiki Andrei <andrei.g.csiki@gmail.com>
2015-08-14 14:56:10 +0300

    Added login.php
    Added login.php and changed name from singIn.html in login.tmpl

---
+++ login.php
@@ -0,0 +1,24 @@
+<?php
+
+// include and register Twig auto-loader
+include 'Twig/Autoloader.php';
+Twig_Autoloader::register();
+
+try {
+	// define template directory location
+	$loader = new Twig_Loader_Filesystem('templates');
+	
+	// initialize Twig environment
+	$twig = new Twig_Environment($loader);
+	
+	// load template
+	$template = $twig->loadTemplate('login.tmpl');
+
+	// set template variables
+	// render template
+	echo $template->render(array ());
+	
+} catch (Exception $e) {
+	die ('ERROR: ' . $e->getMessage());
+}
+?>
