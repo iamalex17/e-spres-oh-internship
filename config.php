d58a2206f5a0d5e36a512e5be4c43607b792e210
Csiki Andrei <andrei.g.csiki@gmail.com>
2015-08-14 17:35:02 +0300

    <login.tmpl> Changed type of password input from text to password. <config.php> DRY-ed some code <login.php> DRY-ed some code <.htacces> Added rewrite module for .php

--- config.php
+++ config.php
@@ -1,14 +1,31 @@
 <?php
 
-	$host = 'localhost';
-	$port = '3306';
-	$user = 'root';
-	$pass = '';
-	$db = 'internship';
+class ConnectDB
+{
+	function __construct($host, $db, $user, $pass)
+	{
+		$this->dbh = new PDO('mysql:host=' . $host . ';dbname=' . $db, $user, $pass);
+	}
+}
 	try {
-		$dbh = new PDO('mysql:host=' . $host . ';dbname=' . $db, $user, $pass);
+		$conn = new ConnectDB('localhost', 'internship', 'root', '');
 	} catch (PDOException $e) {
 		print "Error!: " . $e->getMessage() . "<br/>";
 		die();
 	}
+
+class LoadTemplate
+{
+	function __construct($folder, $templateFile)
+	{
+		// define template directory location
+		$loader = new Twig_Loader_Filesystem($folder);
+		
+		// initialize Twig environment
+		$twig = new Twig_Environment($loader);
+		
+		// load template
+		$this->template = $twig->loadTemplate($templateFile);
+	}
+}
 ?>
--- login.php
+++ login.php
@@ -6,14 +6,7 @@ include 'Twig/Autoloader.php';
 Twig_Autoloader::register();
 
 try {
-	// define template directory location
-	$loader = new Twig_Loader_Filesystem('templates');
-	
-	// initialize Twig environment
-	$twig = new Twig_Environment($loader);
-	
-	// load template
-	$template = $twig->loadTemplate('login.tmpl');
+	$view = new LoadTemplate('templates', 'login.tmpl');
 } catch (Exception $e) {
 	die ('ERROR: ' . $e->getMessage());
 }
@@ -24,18 +17,18 @@ try {
 	if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 		$username = $_POST['username'];
 		$password = $_POST['password'];
-		$sth = $dbh->prepare('SELECT * FROM `users` WHERE username = :username AND password = MD5(:password)');
+		$sth = $conn->dbh->prepare('SELECT * FROM `users` WHERE username = :username AND password = MD5(:password)');
 		$sth->bindValue(':username', $username);
 		$sth->bindValue(':password', $password);
 		$sth->execute();
 		$result = $sth->fetchAll();
 
 		if(count($result)==1){
-			header('Location: dashboard.php');
+			header('Location: dashboard');
 			exit();
 		}
 	}
-	echo $template->render(array('username'=>$username));
+	echo $view->template->render(array('username'=>$username));
 	
 } catch (Exception $e) {
 	die ('ERROR: ' . $e->getMessage());
