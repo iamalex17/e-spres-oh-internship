edb91d145776228aa2bec64f93a33a461553777e
Ungureanu Alex Daniel <ungureanualex17@yahoo.com>
2015-08-14 16:34:27 +0300

    Updated DB
    Modified the foreign key

--- SQL/internship.sql
+++ SQL/internship.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 14, 2015 at 03:10 PM
+-- Generation Time: Aug 14, 2015 at 03:32 PM
 -- Server version: 5.6.24
 -- PHP Version: 5.6.8
 
@@ -31,22 +31,23 @@ CREATE TABLE IF NOT EXISTS `users` (
   `first_name` varchar(20) NOT NULL,
   `last_name` varchar(20) NOT NULL,
   `username` varchar(20) NOT NULL,
-  `email` varchar(50) NOT NULL,
+  `email` varchar(30) NOT NULL,
   `password` varchar(32) NOT NULL,
-  `user_privilege` int(1) unsigned NOT NULL,
-  `status` tinyint(1) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+  `user_privilege` int(1) NOT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `users`
+-- Table structure for table `users_privileges`
 --
 
-INSERT INTO `users` (`id`, `first_name`, `last_name`, `username`, `email`, `password`, `user_privilege`, `status`) VALUES
-(1, 'admin', 'admin', 'admin', 'admin@admin.com', '21232f297a57a5a743894a0e4a801fc3', 1, 1),
-(2, 'Ungureanu', 'Alex', 'alexu', 'ungureanualex17@yahoo.com', '081d29b9330707cc21a1bf4132f7d3f7', 3, 1),
-(3, 'Csiki', 'Andrei', 'andreic', 'andrei.g.csiki@gmail.com', '081d29b9330707cc21a1bf4132f7d3f7', 3, 1),
-(4, 'Pfeiffer', 'Andrei', 'andreip', 'andrei.pfeiffer@e-spres-oh.com', '23cbeacdea458e9ced9807d6cbe2f4d6', 2, 1),
-(5, 'Sitov', 'Cristian', 'cristis', 'cristian.sitov@e-spres-oh.com', '23cbeacdea458e9ced9807d6cbe2f4d6', 2, 1);
+CREATE TABLE IF NOT EXISTS `users_privileges` (
+  `privilege_id` int(3) NOT NULL,
+  `privilege_no` int(1) NOT NULL,
+  `privilege_name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Indexes for dumped tables
@@ -56,7 +57,13 @@ INSERT INTO `users` (`id`, `first_name`, `last_name`, `username`, `email`, `pass
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`), ADD KEY `email` (`email`), ADD KEY `password` (`password`), ADD KEY `user_privilege_FK` (`user_privilege`), ADD KEY `username` (`username`);
+  ADD PRIMARY KEY (`id`), ADD KEY `username` (`username`), ADD KEY `email` (`email`), ADD KEY `user_privilege` (`user_privilege`);
+
+--
+-- Indexes for table `users_privileges`
+--
+ALTER TABLE `users_privileges`
+  ADD PRIMARY KEY (`privilege_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -66,7 +73,12 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(3) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+  MODIFY `id` int(3) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users_privileges`
+--
+ALTER TABLE `users_privileges`
+  MODIFY `privilege_id` int(3) NOT NULL AUTO_INCREMENT;
 --
 -- Constraints for dumped tables
 --
@@ -75,7 +87,7 @@ ALTER TABLE `users`
 -- Constraints for table `users`
 --
 ALTER TABLE `users`
-ADD CONSTRAINT `user_privilege_FK` FOREIGN KEY (`user_privilege`) REFERENCES `users_privileges` (`privilege_no`);
+ADD CONSTRAINT `user_privilege_FK` FOREIGN KEY (`user_privilege`) REFERENCES `users_privileges` (`privilege_id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
