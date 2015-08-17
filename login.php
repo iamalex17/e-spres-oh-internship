15c5d98802aaa5725bdbcdf3c82d263be9651186
Csiki Andrei <andrei.g.csiki@gmail.com>
2015-08-17 16:48:14 +0300

    Removed recover password verification from login.php

--- login.php
+++ login.php
@@ -6,20 +6,16 @@ require_once 'functions/login_functions.php';
 try {
 	$username = '';
 	if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-		if(isset($_POST['email'])){
-			$link = generateRandomString($_POST['email']);
-		}else{
-			$username = $_POST['username'];
-			$password = $_POST['password'];
-			$sth = $dbh->prepare('SELECT * FROM `users` WHERE username = :username AND password = MD5(:password)');
-			$sth->bindValue(':username', $username);
-			$sth->bindValue(':password', $password);
-			$sth->execute();
-			$result = $sth->fetchAll();
-			if(count($result)==1){
-				header('Location: dashboard.php');
-				exit();
-			}	
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		$sth = $dbh->prepare('SELECT * FROM `users` WHERE username = :username AND password = MD5(:password)');
+		$sth->bindValue(':username', $username);
+		$sth->bindValue(':password', $password);
+		$sth->execute();
+		$result = $sth->fetchAll();
+		if(count($result)==1){
+			header('Location: dashboard.php');
+			exit();
 		}
 	}
 	$template = loadTemplate('templates','login.tmpl');
