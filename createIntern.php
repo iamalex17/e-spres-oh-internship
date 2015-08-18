ddb5d36ab1b6d38410e63c21dc13880449cebdb1
Csiki Andrei <andrei.g.csiki@gmail.com>
2015-08-18 15:35:13 +0300

    Added code to verify if email exists or not in DB.

--- createIntern.php
+++ createIntern.php
@@ -12,39 +12,47 @@ try {
 			$firstName = trim($_POST['firstName']);
 			$lastName = trim($_POST['lastName']);
 			$username = trim($_POST['username']);
-			$emailAdress = trim($_POST['emailAdress']);
+			$email = trim($_POST['emailAdress']);
 			$password = trim($_POST['password']);
 			$retypePassword = trim($_POST['retypePassword']);
 			if($firstName == '') {
-				$errorMessage .= 'First Name field not completed properly';
+				$errorMessage .= 'First Name field not completed properly'.PHP_EOL;
 				$status = 0;
 			}
 			if($lastName == '') {
-				$errorMessage .= 'Last Name field not completed properly';
+				$errorMessage .= 'Last Name field not completed properly'.PHP_EOL;
 				$status = 0;
 			}
 			if($password < 8) {
-				$errorMessage = 'Password must have at least 8 characters length';
+				$errorMessage = 'Password must have at least 8 characters length'.PHP_EOL;
 				$status = 0;
 			}
 			if($username == '') {
-				$errorMessage .= 'Username filed not completed properly';
+				$errorMessage .= 'Username filed not completed properly'.PHP_EOL;
 				$status = 0;
 			}
 			if($password != $retypePassword) {
-				$errorMessage .= 'Passwords do not match. Check again!<br>';
+				$errorMessage .= 'Passwords do not match. Check again!'.PHP_EOL;
 				$status = 0;
 				}
-			if(!filter_var($emailAdress, FILTER_VALIDATE_EMAIL)) {
-				$errorMessage .= 'Email is not valid<br>';
+			if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+				$errorMessage .= 'Email is not valid'.PHP_EOL;
+				$status = 0;
+			}else{
+				$sth = $dbh->prepare('SELECT email FROM `users` WHERE email = :email');
+				$sth->bindValue(':email', $email);
+				$sth->execute();
+				if(count($sth->fetchAll())){
+					$errorMessage .= 'Email already exists.'.PHP_EOL;
+				}
 				$status = 0;
 			}
 			if($status == 1) {
-				$sth = $dbh->prepare('INSERT INTO `internship`.`users` (`id`, `first_name`, `last_name`, `username`, `email`, `password`, `user_privilege`, `status`, `reset_password`, `deletion_link_time`) VALUES (NULL, :firstName, :lastName, :emailAdress, :username, MD5(:password), 3, 1, NULL, NULL);');
+				$sth = $dbh->prepare('INSERT INTO `internship`.`users` (`id`, `first_name`, `last_name`, `username`, `email`, `password`, `user_privilege`, `status`, `reset_password`, `deletion_link_time`) VALUES (NULL, :firstName, :lastName, :email, :username, MD5(:password), 3, 1, NULL, NULL);');
 				$sth->bindValue(':firstName', $firstName);
 				$sth->bindValue(':lastName', $lastName);
 				$sth->bindValue(':username', $username);
-				$sth->bindValue(':emailAdress', $emailAdress);
+				$sth->bindValue(':email', $email);
 				$sth->bindValue(':password', $password);
 				$sth->execute();
 				$result = $sth->fetchAll();
