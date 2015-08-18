85a625da8ef8e7c8dcdfa4e1ee7069ed4985c758
Ungureanu Alex Daniel <ungureanualex17@yahoo.com>
2015-08-18 12:02:32 +0300

    Updated DB
    Added delete link column

--- SQL/internship.sql
+++ SQL/internship.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 17, 2015 at 02:51 PM
+-- Generation Time: Aug 18, 2015 at 10:59 AM
 -- Server version: 5.6.24
 -- PHP Version: 5.6.8
 
@@ -35,20 +35,21 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` varchar(32) NOT NULL,
   `user_privilege` int(1) NOT NULL,
   `status` int(1) NOT NULL,
-  `reset_password` char(64) DEFAULT NULL
+  `reset_password` char(64) DEFAULT NULL,
+  `deletion_link_time` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `first_name`, `last_name`, `username`, `email`, `password`, `user_privilege`, `status`, `reset_password`) VALUES
-(1, 'admin', 'admin', 'admin', 'admin@admin.com', '21232f297a57a5a743894a0e4a801fc3', 1, 1, NULL),
-(2, 'Ungureanu', 'Alex', 'ualex', 'ungureanualex17@yahoo.com', '081d29b9330707cc21a1bf4132f7d3f7', 3, 1, NULL),
-(3, 'Csiki', 'Andrei', 'candrei', 'andrei.g.csiki@gmail.com', '081d29b9330707cc21a1bf4132f7d3f7', 3, 1, NULL),
-(4, 'Pfeiffer', 'Andrei', 'pandrei', 'andrei.pfeiffer@e-spres-oh.com', '23cbeacdea458e9ced9807d6cbe2f4d6', 2, 1, NULL),
-(5, 'Sitov', 'Cristian', 'scristi', 'cristian.sitov@e-spres-oh.com', '23cbeacdea458e9ced9807d6cbe2f4d6', 2, 1, NULL),
-(7, 'test', 'test', 'test', 'test@test.com', '098f6bcd4621d373cade4e832627b4f6', 1, 1, 'abc');
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `username`, `email`, `password`, `user_privilege`, `status`, `reset_password`, `deletion_link_time`) VALUES
+(1, 'admin', 'admin', 'admin', 'admin@admin.com', '21232f297a57a5a743894a0e4a801fc3', 1, 1, NULL, NULL),
+(2, 'Ungureanu', 'Alex', 'ualex', 'ungureanualex17@yahoo.com', '081d29b9330707cc21a1bf4132f7d3f7', 3, 1, 'h0TkkOau1W96kZxtJ1AscFYpc0W9HDd3aDZWyCZ9mEsyxm5tyP6LoEeKuUHiQccJ', NULL),
+(3, 'Csiki', 'Andrei', 'candrei', 'andrei.g.csiki@gmail.com', '081d29b9330707cc21a1bf4132f7d3f7', 3, 1, NULL, NULL),
+(4, 'Pfeiffer', 'Andrei', 'pandrei', 'andrei.pfeiffer@e-spres-oh.com', '23cbeacdea458e9ced9807d6cbe2f4d6', 2, 1, NULL, NULL),
+(5, 'Sitov', 'Cristian', 'scristi', 'cristian.sitov@e-spres-oh.com', '23cbeacdea458e9ced9807d6cbe2f4d6', 2, 1, NULL, NULL),
+(7, 'test', 'test', 'test', 'test@test.com', '098f6bcd4621d373cade4e832627b4f6', 1, 1, 'abc', NULL);
 
 -- --------------------------------------------------------
 
