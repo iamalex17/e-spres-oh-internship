41a4c44d8eba87ffb3a44fa15433cafcf4f98a91
Ungureanu Alex Daniel <ungureanualex17@yahoo.com>
2015-08-18 15:46:13 +0300

    Updated DB

--- SQL/internship.sql
+++ SQL/internship.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 18, 2015 at 02:43 PM
+-- Generation Time: Aug 18, 2015 at 02:45 PM
 -- Server version: 5.6.24
 -- PHP Version: 5.6.8
 
