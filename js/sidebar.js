262071b237fecfbf5751e69b67f0491c0056cdaa
kristineVarga <v.kristine.c@gmail.com>
2015-08-19 14:30:57 +0300

    Working Mentor Sidebar

--- js/sidebar.js
+++ js/sidebar.js
@@ -4,10 +4,10 @@ $(document).ready(function() {
 
 	$('.subNav').hide();
 	$('nav').on('click', '.dropDown', function() {
-			$('.subNav').slideToggle('slow');
+		$('.subNav').slideToggle('slow', function() {
+			$('#change-icon').toggleClass('iconMinusMore');
 		});
-
-
+	});
 });
 
 
