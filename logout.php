bb1021c98e89b9ecba38468c3917cfc80b3e5719
Ungureanu Alex Daniel <ungureanualex17@yahoo.com>
2015-08-19 15:26:46 +0300

    On logout cleans session id from DB (set to NULL)

--- logout.php
+++ logout.php
@@ -3,12 +3,15 @@ require_once 'config.php';
 require_once 'functions/load_template.php';
 try {
 	session_start();
-	session_regenerate_id();
+	$userID = $_SESSION['id'];
+	$sth = $dbh->prepare('UPDATE `users` SET session_id = NULL WHERE id = :userID');
+	$sth->bindValue(':userID', $userID);
+	$sth->execute();
 	session_unset();
 	session_destroy();
 	header('Location: login.php');
 	exit();
-	} catch (Exception $e) {
+} catch (Exception $e) {
 	die ('ERROR: ' . $e->getMessage());
 }
 ?>
