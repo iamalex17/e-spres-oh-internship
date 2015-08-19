7667eff12b10092e6f99633cf46a255e0752a7c8
Csiki Andrei <andrei.g.csiki@gmail.com>
2015-08-19 12:05:02 +0300

    Refactored some code.

--- dashboard.php
+++ dashboard.php
@@ -1,16 +1,21 @@
 <?php
 require 'config.php';
 require 'functions/load_template.php';
+
 session_start();
 session_regenerate_id();
+
 $userID = $_SESSION['id'];
 $sth = $dbh->prepare('SELECT last_name, user_privilege FROM `users` WHERE id = :userID');
 $sth->bindValue(':userID', $userID);
 $sth->execute();
 $result = $sth->fetchAll();
-$user_role = $result[0][1];
-$lastName = $result[0][0];
-$template = loadTemplate('templates','dashboard.tmpl');
-echo $template->render(array('user_role'=>$user_role, 'last_name' => $lastName));
-
+if(count($result) == 1){
+	$user = $result[0];
+	$lastName = $user[0];
+	$user_role = $user[1];
+	$template = loadTemplate('templates','dashboard.tmpl');
+	echo $template->render(array('user_role'=>$user_role, 'last_name' => $lastName));
+}
+//else errorMessage?
 ?>
