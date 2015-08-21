7940c8c64511a7112a7d3b430d7faffede6f6458
kristineVarga <v.kristine.c@gmail.com>
2015-08-21 10:19:43 +0300

    Cleanup: Removed code from Remodal.js(in sidebar.js)

--- js/sidebar.js
+++ js/sidebar.js
@@ -9,32 +9,6 @@ $(document).ready(function() {
 	});
 
 
-	$(document).on('opening', '.remodal', function () {
-		console.log('Modal is opening');
-	});
-
-	$(document).on('opened', '.remodal', function () {
-		console.log('Modal is opened');
-	});
-
-	$(document).on('closing', '.remodal', function (e) {
-
-		// Reason: 'confirmation', 'cancellation'
-		console.log('Modal is closing' + (e.reason ? ', reason: ' + e.reason : ''));
-	});
-
-
-	$(document).on('confirmation', '.remodal', function () {
-
-		$('.test').submit();
-		return true;
-	});
-
-	$(document).on('cancellation', '.remodal', function () {
-		return false;
-	});
-
-
 });
 
 
