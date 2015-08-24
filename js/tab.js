bdfb55f67d84a845f6611b94ac937d03fbf2f81a
Andreea Cristescu <andreea_laura_cristescu@yahoo.com>
2015-08-24 16:01:33 +0300

    Clean code for create-course tab (added tabs in js)

--- js/tab.js
+++ js/tab.js
@@ -1,13 +1,10 @@
 jQuery(document).ready(function() {
-    jQuery('.create-menu .tab-links a').on('click', function(e)  {
-        var currentAttrValue = jQuery(this).attr('href');
- 
-        // Show/Hide Tabs
-        jQuery('.create-menu ' + currentAttrValue).show().siblings().hide();
- 
-        // Change/remove current tab to active
-        jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
- 
-        e.preventDefault();
-    });
+	jQuery('.create-menu .tab-links a').on('click', function(e)  {
+		var currentAttrValue = jQuery(this).attr('href');
+// Show/Hide Tabs
+		jQuery('.create-menu ' + currentAttrValue).show().siblings().hide();
+// Change/remove current tab to active
+		jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
+		e.preventDefault();
+	});
 });
