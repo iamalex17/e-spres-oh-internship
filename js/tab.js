331b98b13619d5017c64836c7effccbe9a07d196
Andreea Cristescu <andreea_laura_cristescu@yahoo.com>
2015-08-24 15:57:39 +0300

    Added functionality to the create-course tab

--- js/tab.js
+++ js/tab.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(function() {
-    jQuery('.tabs .tab-links a').on('click', function(e)  {
+    jQuery('.create-menu .tab-links a').on('click', function(e)  {
         var currentAttrValue = jQuery(this).attr('href');
  
         // Show/Hide Tabs
-        jQuery('.tabs ' + currentAttrValue).show().siblings().hide();
+        jQuery('.create-menu ' + currentAttrValue).show().siblings().hide();
  
         // Change/remove current tab to active
         jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
