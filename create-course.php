ec4fef58ac14226227dd08681dac071289ceb894
Ungureanu Alex Daniel <ungureanualex17@yahoo.com>
2015-08-25 16:56:31 +0300

    Added functionality to display mentors in courses dynamically

--- create-course.php
+++ create-course.php
@@ -1,7 +1,30 @@
 <?php
-require 'config.php';
-require 'functions/load-template.php';
+require_once 'config.php';
+require_once 'functions/load-template.php';
+require_once 'class.connect-to-db.php';
+require_once 'class.user.php';
 
-$template = loadTemplate('templates', 'create-course.tmpl');
-echo $template->render(array());
+if(!User::verifySessionID()) {
+	header('Location: login.php');
+	exit();
+}
+
+$errorMessage = '';
+
+if(isset($_SESSION['errorMessage'])) {
+	$errorMessage = $_SESSION['errorMessage'];
+	unset($_SESSION['errorMessage']);
+}
+
+try {
+	$user = new User($_SESSION);
+	$sql = 'SELECT id, last_name FROM `users` WHERE user_role = 2 AND status = 1';
+	$mentor = ConnectToDB::interogateDB($sql);
+	/*var_dump($mentor);
+	exit();*/
+	$template = loadTemplate('templates','create-course.tmpl');
+	echo $template->render(array('last_name' => $user->last_name, 'profile_image' => $user->profile_image, 'errorMessage' => $errorMessage, 'mentor' => $mentor));
+} catch (Exception $e) {
+	die('ERROR: ' . $e->getMessage());
+}
 ?>
