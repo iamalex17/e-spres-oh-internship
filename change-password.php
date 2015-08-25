87d61c8b1abf2cf39d448de428d3997fccca1b7a
Ungureanu Alex Daniel <ungureanualex17@yahoo.com>
2015-08-25 14:58:22 +0300

    Added functionality to change password (mentor)

--- change-password.php
+++ change-password.php
@@ -1,14 +1,26 @@
 <?php
 require_once 'config.php';
 require_once 'functions/load-template.php';
-if(!verifySessionID($dbh)) {
+require_once 'class.connect-to-db.php';
+require_once 'class.user.php';
+
+if(!User::verifySessionID()) {
 	header('Location: login.php');
 	exit();
 }
-$userID = $_SESSION['id'];
-$lastName = $_SESSION['last_name'];
-$userRole = $_SESSION['user_privilege'];
-$profileImage = $_SESSION['profile_image'];
-$template = loadTemplate('templates','change-password.tmpl');
-echo $template->render(array('userID' => $userID, 'last_name' => $lastName, 'user_role' => $userRole, 'profile_image' => $profileImage));
+
+$errorMessage = '';
+
+if(isset($_SESSION['errorMessage'])) {
+	$errorMessage = $_SESSION['errorMessage'];
+	unset($_SESSION['errorMessage']);
+}
+
+try {
+	$user = new User($_SESSION);
+	$template = loadTemplate('templates','change-password.tmpl');
+	echo $template->render(array('last_name' => $user->last_name, 'profile_image' => $user->profile_image, 'errorMessage' => $errorMessage));
+} catch (Exception $e) {
+	die('ERROR: ' . $e->getMessage());
+}
 ?>
