1f4b56ad6bda79252f5e7a4b024fbac9c42af30b
Ungureanu Alex Daniel <ungureanualex17@yahoo.com>
2015-08-27 19:40:33 +0300

    Added functionality to the delete course button

---
+++ controllers/delete-course.php
@@ -0,0 +1,22 @@
+<?php
+require_once '../config.php';
+require_once '../classes/class.user.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	session_start();
+	$request = User::verifyRequestURL($_SERVER['HTTP_REFERER']);
+	if(($request != 'dashboard.php') || ($_SESSION['user_role'] != 2)) {
+		header('Location: ../dashboard.php');
+		exit();
+	}
+	$deleteCourse = $_POST['delete_course'];
+	$sql = 'UPDATE `courses` SET status = 0 WHERE id = :deletedCourseID';
+	$valuesToBind = array('deletedCourseID' => $deleteCourse);
+	ConnectToDB::interogateDB($sql, $valuesToBind);
+	$successMessage = 'Course succesfully deleted!';
+	$_SESSION['successMessage'] = $successMessage;
+	header('Location: ../dashboard.php');
+	exit();
+}
+
+?>
