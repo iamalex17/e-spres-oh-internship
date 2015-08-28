2508519711b50d72bb7e08fca13dbd7b47c1b1b1
kristineVarga <v.kristine.c@gmail.com>
2015-08-28 13:42:37 +0300

    Clean: Moved TinyMCE initialization script in external file

--- js/tab.js
+++ js/tab.js
@@ -7,4 +7,14 @@ jQuery(document).ready(function() {
 		jQuery(this).parent('li').addClass('active').siblings().removeClass('active').addClass('inactive');
 		e.preventDefault();
 	});
+
+	tinymce.init({
+			selector: "textarea",
+			plugins: [
+				"advlist autolink lists link image charmap print preview anchor",
+				"searchreplace visualblocks code fullscreen",
+				"insertdatetime media table contextmenu paste"
+			],
+			toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+		});
 });
