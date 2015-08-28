773c641a1a48f92c095f12b637cf74364436b65d
Csiki Andrei <andrei.g.csiki@gmail.com>
2015-08-28 13:06:37 +0300

    Fix: Fixed edit course insert into DB error.

--- controllers/edit-a-course.php
+++ controllers/edit-a-course.php
@@ -1,28 +1,18 @@
 <?php
 require_once '../config.php';
+require_once '../controllers/load-template.php';
 require_once '../classes/class.connect-to-db.php';
 require_once '../classes/class.user.php';
-require_once '../controllers/load-template.php';
 
-session_start();
-$sql = 'SELECT * FROM `courses` WHERE id = :id';
-$id = $_GET['course_id'];
-$valuesToBind = array('id' => $id);
-$result = ConnectToDB::interogateDB($sql, $valuesToBind);
-$titleValue = $result[0]['title'];
-$descriptionValue = $result[0]['description'];
-//$labelValue = $result[0]['label'];
-$sql = 'SELECT id, last_name, user_role FROM `users` WHERE user_role = 2 AND status = 1';
-$mentor = ConnectToDB::interogateDB($sql);
-
-$user = new User($_SESSION);
-$template = loadTemplate('../templates','create-course.tmpl');
-echo $template->render(array('last_name' => $user->last_name, 'profile_image' => $user->profile_image, 'user_role' => $user->user_role, 'titleValue' => $titleValue, 'descriptionValue' => $descriptionValue, 'mentor' => $mentor));
-
+if(!User::verifySessionID()) {
+	header('Location: login.php');
+	exit();
+}
 
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
 	$status = 1;
 	$errorMessage = '';
+	$id = $_POST['course_id'];
 	if(!isset($_POST['title'])) {
 		$_POST['title'] = trim($_POST['title']);
 		$errorMessage .= "Add a title to the course.\n";
@@ -33,7 +23,6 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 		$errorMessage .= "Select a label: backend or frontend.\n";
 		$status = 0;
 	}
-
 	if(empty($_POST['mentor'])) {
 		$errorMessage .= "Choose at least one mentor.\n";
 		$status = 0;
@@ -55,22 +44,32 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 		$status = 0;
 	}
 	
-	$sql = 'SELECT title FROM `courses`';
-	$result = ConnectToDB::interogateDB($sql);
-	if(strtolower($_POST['title']) == strtolower($result[0][0])) {
-		$errorMessage = "A course with this name already exists\n";
+	$sql = 'SELECT count(*) FROM `courses` WHERE title = :title AND id != :id';
+	$valuesToBind = array('title' => $_POST['title'], 'id' => $id);
+	$result = ConnectToDB::interogateDB($sql, $valuesToBind);
+	if($result[0][0]) {
 		$status = 0;
 	}
-	
+
 	if($status == 1) {
 		$title = $_POST['title'];
 		$label = $_POST['label'];
 		$presentors = $_POST['mentor'];
 		$description = $_POST['textareas'];
 		$sql = 'UPDATE `courses` SET title = :title, label = :label, description = :description WHERE id = :id';
-		$valuesToBind = array('title' => $title, 'label' => $label, 'textareas' => $textareas, 'id' => $id);
+		$valuesToBind = array('title' => $title, 'label' => $label, 'description' => $description, 'id' => $id);
 		$result = ConnectToDB::interogateDB($sql, $valuesToBind);
-		
+
+		$sql = 'DELETE FROM `presentors` WHERE course_id = :id';
+		$valuesToBind = array('id' => $id);
+		ConnectToDB::interogateDB($sql, $valuesToBind);
+
+		foreach ($presentors as $presentor) {
+			$sql = 'INSERT INTO `presentors` (course_id, presentor_id) VALUES (:course_id, :presentor_id)';
+			$valuesToBind = array('course_id' => $id, 'presentor_id' => $presentor);
+			ConnectToDB::interogateDB($sql, $valuesToBind);
+		}
+
 		$successMessage = "Course updated!\n";
 		$_SESSION['successMessage'] = $successMessage;
 		header('Location: ../dashboard.php');
--- users/create-course.php
+++ users/create-course.php
@@ -10,18 +10,44 @@ if(!User::verifySessionID()) {
 }
 
 $errorMessage = '';
-
 if(isset($_SESSION['errorMessage'])) {
 	$errorMessage = $_SESSION['errorMessage'];
 	unset($_SESSION['errorMessage']);
 }
 
 try {
+	$course = '';
+	$courseMentors = '';
+	$page = '';
+	if(($_SERVER['REQUEST_METHOD'] == 'GET') && isset($_GET['course_id'])) {
+		$page = 'edit';
+		$request = User::verifyRequestURL($_SERVER['HTTP_REFERER']);
+		if($request != 'dashboard.php') {
+			header('Location: ../dashboard.php');
+			exit();
+		}
+		$sql = 'SELECT * FROM `courses` WHERE id = :id';
+		$id = $_GET['course_id'];
+		$valuesToBind = array('id' => $id);
+		$result = ConnectToDB::interogateDB($sql, $valuesToBind);
+		$course = $result[0];
+		$sql = 'SELECT id, last_name, user_role FROM `users` WHERE user_role = 2 AND status = 1';
+		$mentor = ConnectToDB::interogateDB($sql);
+
+		$sql = 'SELECT users.id
+				FROM `users`
+				INNER JOIN `presentors` ON `presentors`.`presentor_id` = `users`.`id`
+				INNER JOIN `courses` ON `courses`.`id` = `presentors`.`course_id`
+				WHERE `courses`.`id` = :courseID';
+		$valuesToBind = array('courseID' => $course['id']);
+		$courseMentors = ConnectToDB::interogateDB($sql, $valuesToBind);
+	}
+
 	$user = new User($_SESSION);
 	$sql = 'SELECT id, last_name, user_role FROM `users` WHERE user_role = 2 AND status = 1';
 	$mentor = ConnectToDB::interogateDB($sql);
 	$template = loadTemplate('../templates','create-course.tmpl');
-	echo $template->render(array('last_name' => $user->last_name, 'profile_image' => $user->profile_image, 'errorMessage' => $errorMessage, 'mentor' => $mentor, 'user_role' => $user->user_role));
+	echo $template->render(array('last_name' => $user->last_name, 'profile_image' => $user->profile_image, 'user_role' => $user->user_role, 'mentor' => $mentor, 'course' => $course, 'courseMentors' => $courseMentors, 'page' => $page, 'errorMessage' => $errorMessage));
 } catch (Exception $e) {
 	die('ERROR: ' . $e->getMessage());
 }
