f15bb388a52185e61a54a0e8337bf64547275e10
Ungureanu Alex Daniel <ungureanualex17@yahoo.com>
2015-08-31 14:27:20 +0300

    Updated DB column label to have both labels selected

--- SQL/internship.sql
+++ SQL/internship.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 27, 2015 at 02:12 PM
+-- Generation Time: Aug 31, 2015 at 01:26 PM
 -- Server version: 5.6.24
 -- PHP Version: 5.6.8
 
@@ -29,18 +29,22 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `courses` (
   `id` int(3) NOT NULL,
   `title` varchar(30) NOT NULL,
-  `label` varchar(15) NOT NULL,
+  `label` varchar(20) NOT NULL,
   `description` blob,
   `status` int(2) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `courses`
 --
 
 INSERT INTO `courses` (`id`, `title`, `label`, `description`, `status`) VALUES
-(1, 'HTML', 'frontend', 0x3c703e637572732068746d6c3c2f703e, 1),
-(2, 'test', 'Frontend', 0x3c703e746573743c2f703e, 1);
+(1, 'HTML', 'frontend', 0x3c703e637572732068746d6c3c2f703e, 0),
+(2, 'test', 'Frontend', 0x3c703e746573743c2f703e, 0),
+(3, 'Javascript', 'Frontend', 0x3c703e63757273206a733c2f703e, 1),
+(8, 'test', 'Frontend', 0x3c703e746573743c2f703e, 0),
+(9, 'SQL', 'Backend', 0x3c703e637572732073716c3c2f703e, 1),
+(10, 'Beta', 'Frontend, Backend', 0x3c703e6371776975746f69676e746a663c2f703e, 1);
 
 -- --------------------------------------------------------
 
@@ -52,7 +56,7 @@ CREATE TABLE IF NOT EXISTS `presentors` (
   `id` int(3) NOT NULL,
   `course_id` int(3) NOT NULL,
   `presentor_id` int(3) unsigned NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `presentors`
@@ -60,7 +64,11 @@ CREATE TABLE IF NOT EXISTS `presentors` (
 
 INSERT INTO `presentors` (`id`, `course_id`, `presentor_id`) VALUES
 (1, 1, 4),
-(2, 2, 4);
+(2, 2, 4),
+(3, 3, 4),
+(10, 2, 5),
+(11, 9, 5),
+(12, 10, 4);
 
 -- --------------------------------------------------------
 
@@ -88,10 +96,10 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 INSERT INTO `users` (`id`, `first_name`, `last_name`, `username`, `email`, `password`, `user_role`, `profile_image`, `session_id`, `status`, `reset_password`, `deletion_link_time`) VALUES
-(1, 'admin', 'admin', 'admin', 'admin@admin.com', '21232f297a57a5a743894a0e4a801fc3', 1, NULL, 'rl0gplhn1o3f533ru0n5bapq83', 1, NULL, NULL),
-(2, 'Ungureanu', 'Alex', 'ualex', 'ungureanualex17@yahoo.com', '081d29b9330707cc21a1bf4132f7d3f7', 3, NULL, 'ihg6c1hlkmudoccb1q6oh59b20', 1, NULL, NULL),
+(1, 'admin', 'admin', 'admin', 'admin@admin.com', '21232f297a57a5a743894a0e4a801fc3', 1, NULL, 'sp8jg4q5krnb7m5l63e916cr43', 1, NULL, NULL),
+(2, 'Ungureanu', 'Alex', 'ualex', 'ungureanualex17@yahoo.com', '081d29b9330707cc21a1bf4132f7d3f7', 3, NULL, 'fvout77q7sbim7g9qbqeuiqsm0', 1, NULL, NULL),
 (3, 'Csiki', 'Andrei', 'candrei', 'andrei.g.csiki@gmail.com', '081d29b9330707cc21a1bf4132f7d3f7', 3, NULL, NULL, 1, NULL, NULL),
-(4, 'Pfeiffer', 'Andrei', 'pandrei', 'andrei.pfeiffer@e-spres-oh.com', '23cbeacdea458e9ced9807d6cbe2f4d6', 2, '21232f297a57a5a743894a0e4a801fc3.png', '9254aqklnrum04uf7nu7fk0pp4', 1, NULL, NULL),
+(4, 'Pfeiffer', 'Andrei', 'pandrei', 'andrei.pfeiffer@e-spres-oh.com', '23cbeacdea458e9ced9807d6cbe2f4d6', 2, '21232f297a57a5a743894a0e4a801fc3.png', 'o7s5blhttccltt3bo74ppm6pi5', 1, NULL, NULL),
 (5, 'Sitov', 'Cristian', 'scristi', 'cristian.sitov@e-spres-oh.com', '23cbeacdea458e9ced9807d6cbe2f4d6', 2, NULL, NULL, 1, NULL, NULL),
 (6, 'test', 'test', 'test', 'test@yahoo.com', '05a671c66aefea124cc08b76ea6d30bb', 2, '21232f297a57a5a743894a0e4a801fc3.png', NULL, 1, NULL, NULL),
 (7, 'test', 'test', 'test1', 'test@saf.com', '05a671c66aefea124cc08b76ea6d30bb', 3, '21232f297a57a5a743894a0e4a801fc3.png', NULL, 1, NULL, NULL);
@@ -153,12 +161,12 @@ ALTER TABLE `users_privileges`
 -- AUTO_INCREMENT for table `courses`
 --
 ALTER TABLE `courses`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT for table `presentors`
 --
 ALTER TABLE `presentors`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
 --
 -- AUTO_INCREMENT for table `users`
 --
