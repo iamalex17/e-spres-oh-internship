b9e19f073ae3e8770a597506438d1ea43fd4e200
Csiki Andrei <andrei.g.csiki@gmail.com>
2015-09-02 12:10:24 +0300

    Added user verification functions.

--- controllers/create-exercise.php
+++ controllers/create-exercise.php
@@ -1,3 +1,22 @@
 <?php
+require_once '../config.php';
+require_once '../controllers/load-template.php';
+require_once '../classes/class.connect-to-db.php';
+require_once '../classes/class.user.php';
+require_once '../classes/class.exercise.php';
 
+if(!User::verifySessionID()) {
+	header('Location: ../login.php');
+	exit();
+}
+
+if(($_SERVER['REQUEST_METHOD'] == 'POST') {
+	$request = User::verifyRequestURL($_SERVER['HTTP_REFERER']);
+	if($request != 'create-course.php') {
+		header('Location: ../dashboard.php');
+		exit();
+	}
+}
+
+var_dump($_POST);
 ?>
