d205374804c99e73d1398fa4d2725050c4d18210
kristineVarga <v.kristine.c@gmail.com>
2015-09-02 17:07:00 +0300

    Fix: Initiate new TinyMCE textarea on Add Exercise button

--- js/tab.js
+++ js/tab.js
@@ -1,15 +1,20 @@
 $(document).ready(function() {
 	$('.create-menu .tab-links a').on('click', function(e)  {
 		var currentAttrValue = $(this).attr('href');
-// Show/Hide Tabs
+		// Show/Hide Tabs
 		$('.create-menu ' + currentAttrValue).show().siblings().hide();
-// Change/remove current tab to active
+		// Change/remove current tab to active
 		$(this).parent('li').addClass('active').siblings().removeClass('active').addClass('inactive');
 		e.preventDefault();
 	});
 
-	tinymce.init({
+	initTinyMCE();
+
+	function initTinyMCE() {
+		tinymce.init({
 			selector: "textarea",
+			mode: "textareas",
+			theme_advanced_path: false,
 			plugins: [
 				"advlist autolink lists link image charmap print preview anchor",
 				"searchreplace visualblocks code fullscreen",
@@ -17,9 +22,14 @@ $(document).ready(function() {
 			],
 			toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
 		});
+	}
 
-	$('#tab2').on('click', '#buttonAddExercise', function(e) {
-		e.preventDefault();
-		$('.create-exercise').last().clone().appendTo('.create-exercise-container');
+	$('#tab2').on('click', '#buttonAddExercise', function() {
+		var ta_count = $("textarea").length;
+		var elem = document.createElement("textarea");
+		$(elem).attr("id", ta_count.toString());
+		$(elem).appendTo(".create-exercise-container");
+
+		initTinyMCE();
 	});
 });
