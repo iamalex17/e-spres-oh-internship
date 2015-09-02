9001d59e4e39d1c9eed12e003620849c848a744f
Ungureanu Alex Daniel <ungureanualex17@yahoo.com>
2015-09-02 15:26:48 +0300

    Fix: submitted exercises sidebar works properly + path dynamically

--- users/submitted-exercises.php
+++ users/submitted-exercises.php
@@ -4,7 +4,13 @@ require_once '../controllers/load-template.php';
 require_once '../classes/class.connect-to-db.php';
 require_once '../classes/class.user.php';
 
+if(!User::verifySessionID()) {
+	header('Location: ' . $GLOBALS['path'] . 'login.php');
+	exit();
+}
 
-	$template = loadTemplate('../templates','submitted-exercises.tmpl');
-	echo $template->render(array());
+$user = new User($_SESSION);
+
+$template = loadTemplate('../templates','submitted-exercises.tmpl');
+echo $template->render(array('path' => $path, 'last_name' => $user->last_name, 'profile_image' => $user->profile_image, 'user_role' => $user->user_role));
 ?>
