fe6971ec34c5ac6eb3fe3e2eddd2ceae6bb736c1
Csiki Andrei <andrei.g.csiki@gmail.com>
2015-09-02 14:22:34 +0300

    Fix: Fixed config when accessed from production.

--- config.php
+++ config.php
@@ -1,20 +1,36 @@
 <?php
 	require_once 'classes/class.connect-to-db.php';
 	require_once 'classes/class.user.php';
-//	$interogate = 'ConnectToDB::interogateDB';
-	$host = 'localhost';
-	$db = 'internship';
-	$user = 'root';
-	$pass = '';
-	try {
-		$dbConnector = new ConnectToDB($host, $db, $user, $pass);
-	} catch (PDOException $e) {
-		print "Error!: " . $e->getMessage() . "<br/>";
-		die();
+
+	if(($_SERVER['SERVER_NAME'] == '127.0.0.1') || $_SERVER['SERVER_NAME'] == 'localhost') {
+		$host = 'localhost';
+		$db = 'internship';
+		$user = 'root';
+		$pass = '';
+		try {
+			$dbConnector = new ConnectToDB($host, $db, $user, $pass);
+		} catch (PDOException $e) {
+			print "Error!: " . $e->getMessage() . "<br/>";
+			die();
+		}
+
+		$server = 'http://' . $_SERVER['SERVER_NAME'] . '/';
+		$additionalFolder = 'e-spres-oh-internship/';
+		$path = $server . $additionalFolder;
+
+	} else if ($_SERVER['SERVER_NAME'] == 'esohintern.bucatzel.ro') {
+		$host = 'localhost';
+		$db = 'esohintern';
+		$user = 'esohintern';
+		$pass = 'eI--sjdf1';
+		try {
+			$dbConnector = new ConnectToDB($host, $db, $user, $pass);
+		} catch (PDOException $e) {
+			print "Error!: " . $e->getMessage() . "<br/>";
+			die();
+		}
+		$server = 'http://' . $_SERVER['SERVER_NAME'] . '/';
+		$additionalFolder = '';
+		$path = $server . $additionalFolder;
 	}
-
-	$server = 'http://' . $_SERVER['SERVER_NAME'] . '/';
-	$additionalFolder = 'e-spres-oh-internship/';
-
-	$path = $server . $additionalFolder;
 ?>
