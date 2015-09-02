9f2976d629731bfb64ba0ce4b4b523a5bd8682f8
Csiki Andrei <andrei.g.csiki@gmail.com>
2015-09-02 18:05:40 +0300

    Added view exercises functionality in course details page.

--- controllers/course-details.php
+++ controllers/course-details.php
@@ -1,12 +1,23 @@
 <?php
 
-
+	$exercisesMessage = '';
 	$course_id = $_GET['course_id'];
 
 	$sql = 'SELECT * FROM `courses` WHERE id = :id';
 	$valuesToBind = array('id'=>$course_id);
 	$result = ConnectToDB::interogateDB($sql, $valuesToBind);
 	$course = $result[0];
+	$sql = 'SELECT * FROM `exercises` WHERE course_id = :courseID';
+	$valuesToBind = array('courseID' => $course['id']);
+	$exercises = ConnectToDB::interogateDB($sql, $valuesToBind);
+	if(count($exercises)) {
+		foreach ($exercises as &$exercise) {
+			$exercise['description'] = html_entity_decode($exercise['description']);
+		}
+		$course['exercises'] = $exercises;
+	} else {
+		$exercisesMessage = 'No exercise at the moment.';
+	}
 	$template = loadTemplate('../templates','details-course.tmpl');
-	echo $template->render(array('user_role' => $_SESSION['user_role'], 'last_name' => $_SESSION['last_name'], 'profile_image' => $_SESSION['profile_image'], 'course' => $course, 'path' => $path));
+	echo $template->render(array('exercisesMessage'=> $exercisesMessage, 'user_role' => $_SESSION['user_role'], 'last_name' => $_SESSION['last_name'], 'profile_image' => $_SESSION['profile_image'], 'course' => $course, 'path' => $path));
 ?>
