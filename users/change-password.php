cccc65083fbab8429fb7b4edfca6b34dfc7c04c0
Ungureanu Alex Daniel <ungureanualex17@yahoo.com>
2015-09-02 17:58:24 +0300

    Added functionality to submitted exercises on change password

--- users/change-password.php
+++ users/change-password.php
@@ -16,10 +16,13 @@ if(isset($_SESSION['errorMessage'])) {
 	unset($_SESSION['errorMessage']);
 }
 
+$sql = 'SELECT `c`.`title`, `c`.`id` FROM `courses` `c` WHERE (SELECT count(*) FROM exercises WHERE course_id = `c`.`id`) > 0';
+$coursesWithExercises = ConnectToDB::interogateDB($sql);
+
 try {
 	$user = new User($_SESSION);
 	$template = loadTemplate('../templates','change-password.tmpl');
-	echo $template->render(array('last_name' => $user->last_name, 'profile_image' => $user->profile_image, 'errorMessage' => $errorMessage, 'user_role' => $user->user_role, 'path' => $path));
+	echo $template->render(array('last_name' => $user->last_name, 'profile_image' => $user->profile_image, 'errorMessage' => $errorMessage, 'user_role' => $user->user_role, 'path' => $path, 'coursesWithExercises' => $coursesWithExercises));
 } catch (Exception $e) {
 	die('ERROR: ' . $e->getMessage());
 }
