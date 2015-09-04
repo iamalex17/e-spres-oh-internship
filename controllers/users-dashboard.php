ac20b51d89b3b4e81e6e6460bdf80153624af243
Csiki Andrei <andrei.g.csiki@gmail.com>
2015-09-04 12:38:44 +0300

    Fix: Only courses with exercises are displayed on submitted exercises.

--- controllers/users-dashboard.php
+++ controllers/users-dashboard.php
@@ -10,9 +10,8 @@
 	$sql = 'SELECT * FROM `courses` WHERE status = 0';
 	$deletedCourses = ConnectToDB::interogateDB($sql);
 
-	$sql = 'SELECT `c`.`title`, `c`.`id`, `c`.`status` FROM `courses` `c` WHERE (SELECT count(*) FROM exercises WHERE course_id = `c`.`id`) > 0 AND status = 1';
+	$sql = 'SELECT `c`.`title`, `c`.`id`, `c`.`status` FROM `courses` `c` WHERE (SELECT count(*) FROM exercises WHERE course_id = `c`.`id` AND `exercises`.`status` = 1) > 0 AND status = 1';
 	$coursesWithExercises = ConnectToDB::interogateDB($sql);
-
 //	$sql = 'SELECT `c`.`title`, `c`.`id`, `c`.`status` FROM `courses` `c` WHERE (SELECT count(*) FROM exercises WHERE course_id = `c`.`id`) > 0 AND status = 0';
 //	$coursesWithoutExercises = ConnectToDB::interogateDB($sql);
 
