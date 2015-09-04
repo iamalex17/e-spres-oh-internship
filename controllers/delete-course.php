06a34112b8b096e710bbcc71a1b44c7887602a1f
Csiki Andrei <andrei.g.csiki@gmail.com>
2015-09-04 10:31:47 +0300

    Fix: When a course is deleted also exercises and solutions of that course are deleted.

--- controllers/delete-course.php
+++ controllers/delete-course.php
@@ -13,6 +13,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	$sql = 'UPDATE `courses` SET status = 0 WHERE id = :deletedCourseID';
 	$valuesToBind = array('deletedCourseID' => $deleteCourse);
 	ConnectToDB::interogateDB($sql, $valuesToBind);
+	$sql = 'UPDATE `exercises` SET status = 0 WHERE course_id = :courseID';
+	$valuesToBind = array('courseID' => $deleteCourse);
+	ConnectToDB::interogateDB($sql, $valuesToBind);
+	$sql = 'UPDATE `submitted_exercises` SET status = 0 WHERE exercise_id = (SELECT id FROM `exercises` WHERE course_id = :courseID)';
+	$valuesToBind = array('courseID' => $deleteCourse);
+	ConnectToDB::interogateDB($sql, $valuesToBind);
 	$successMessage = 'Course succesfully deleted!';
 	$_SESSION['successMessage'] = $successMessage;
 	header('Location: ' . $GLOBALS['path'] . 'dashboard.php');
