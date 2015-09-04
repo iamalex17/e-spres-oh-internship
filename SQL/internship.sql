85ec1ac8b6027dc3d6004016ef2539955fa6a2ff
Ungureanu Alex Daniel <ungureanualex17@yahoo.com>
2015-09-04 14:21:19 +0300

    Exported latest DB with real data

--- SQL/internship.sql
+++ SQL/internship.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 04, 2015 at 01:17 PM
+-- Generation Time: Sep 04, 2015 at 01:19 PM
 -- Server version: 5.6.24
 -- PHP Version: 5.6.8
 
