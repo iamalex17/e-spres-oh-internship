0c2f5e06e12bdea0a25e4b71abb9c4f4f213da82
Andreea Cristescu <andreea_laura_cristescu@yahoo.com>
2015-09-07 14:29:34 +0300

    Bug Fix: Modied the classes and added margins on headings in change password/edit profile template

--- js/tab.js
+++ js/tab.js
@@ -36,13 +36,13 @@ $(document).ready(function() {
 			selector: "textarea",
 			mode: "textareas",
 			theme_advanced_path: true,
-			content_css : "../css/normalize.css",
+			content_css : "../css/custom-content.css",
 			plugins: [
 				"advlist autolink lists link charmap print preview anchor",
 				"searchreplace visualblocks code fullscreen",
 				"insertdatetime media table contextmenu paste"
 			],
-			toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image fontselect fontsizeselect"
+			toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image fontsizeselect"
 		});
 	}
 
