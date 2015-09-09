5dde37f8a9411f2aade3fc3a0aa48e167859dab8
Csiki Andrei <andrei.g.csiki@gmail.com>
2015-09-09 11:36:41 +0300

    Added: If reset password link doesn't exist or is expired users will be redirected and an error message will be shown.

--- users/reset-password.php
+++ users/reset-password.php
@@ -1,15 +1,32 @@
 <?php
 require_once '../config.php';
+require_once '../classes/class.connect-to-db.php';
 require_once '../controllers/load-template.php';
 try {
 
-$link = '';
+session_start();
 
+$link = '';
 if($_SERVER['REQUEST_METHOD'] == 'GET') {
 	$link = isset($_GET["link"]) ? trim($_GET["link"]) : '';
+	$sql = 'SELECT reset_password, deletion_link_time FROM `users` WHERE reset_password = :resetPassword';
+	$valuesToBind = array('resetPassword' => $link);
+	$result = ConnectToDB::interogateDB($sql, $valuesToBind);
+	if(!count($result)) {
+		$_SESSION['errorMessage'] = 'Requested link not found.';
+		header('Location:' . $path . 'login.php');
+		exit();
+	} else {
+		$createDate = strtotime( $result[0]['deletion_link_time'] );
+		$expireDate = $createDate + 86400;
+		$currentdate = strtotime(date('Y/m/d h:i:s', time()));
+		if ($expireDate - $currentdate < 0) {
+			$_SESSION['errorMessage'] = "This link expired.\nPlease re-enter your email.";
+			header('Location:' . $path . 'users/recover-password.php');
+			exit();
+		}
+	}
 }
-
-session_start();
 
 $errorMessage = '';
 
