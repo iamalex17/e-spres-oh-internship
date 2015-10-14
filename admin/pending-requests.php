38c3e054ac9e73e86766b7fe6cf016ec4226f717
iamalex17 <ungureanualex17@yahoo.com>
2015-10-14 14:15:25 +0300

    Added feature to display pending users

--- admin/pending-requests.php
+++ admin/pending-requests.php
@@ -5,11 +5,16 @@ require_once '../classes/class.connect-to-db.php';
 require_once '../classes/class.user.php';
 
 $errorMessage = '';
-$userToAdd = '';
+$noRequests = '';
 
 if(!User::verifySessionID()) {
 	header('Location: ' . $GLOBALS['path'] . 'login.php');
 	exit();
+}
+
+if(isset($_SESSION['noRequests'])) {
+	$errorMessage = $_SESSION['noRequests'];
+	unset($_SESSION['noRequests']);
 }
 
 if(isset($_SESSION['errorMessage'])) {
@@ -17,15 +22,17 @@ if(isset($_SESSION['errorMessage'])) {
 	unset($_SESSION['errorMessage']);
 }
 
-if(isset($_SESSION['userToAdd'])) {
-	$userToAdd = $_SESSION['userToAdd'];
-	unset($_SESSION['userToAdd']);
+$sql = 'SELECT * FROM `google_users` WHERE status = 0 AND user_role IS NULL';
+$pendingUsers = ConnectToDB::interogateDB($sql);
+
+if(count($pendingUsers) == 0) {
+	$noRequests = "No users at the moment.\n";
 }
 
 try {
 	$user = new User($_SESSION);
 	$template = loadTemplate('../templates', 'pending-requests.tmpl');
-	echo $template->render(array('id' => $user->id, 'first_name' => $user->first_name, 'user_role' => $user->user_role, 'profile_image' => $user->profile_image, 'errorMessage' => $errorMessage, 'userToAdd' => $userToAdd, 'path' => $path, 'currentPage' => $currentPage));
+	echo $template->render(array('id' => $user->id, 'first_name' => $user->first_name, 'user_role' => $user->user_role, 'profile_image' => $user->profile_image, 'errorMessage' => $errorMessage, 'path' => $path, 'currentPage' => $currentPage, 'pendingUsers' => $pendingUsers, 'noRequests' => $noRequests));
 } catch (Exception $e) {
 	die ('ERROR: ' . $e->getMessage());
 }
