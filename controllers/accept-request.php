f7a6b185598905c0c88bffb1d25a6997d413321b
iamalex17 <ungureanualex17@yahoo.com>
2015-10-19 17:45:43 +0300

    Uncommented the functionality to receive an email if the admin accepted your request

--- controllers/accept-request.php
+++ controllers/accept-request.php
@@ -32,21 +32,21 @@ if($status == 1) {
 	$sql = 'SELECT google_email FROM `google_users` WHERE google_id = :google_id';
 	$valuesToBind = array('google_id' => $_POST['accept_button']);
 	$email = ConnectToDB::interogateDB($sql, $valuesToBind);
-//	$sendgrid_username = "internship-espresoh";
-//	$sendgrid_password = "internship-project1";
-//	$emailContent = "Hello there (: <br> Looks like your account have been aproved by the admin. Excited? <br> You can access the application now at http://esohintern.bucatzel.ro/login.php. <br> Enjoy and have a nice day!";
-//	$to = $email[0]['google_email'];
-//	$from = 'lex.greenapple@gmail.com';
-//	$sendgrid = new SendGrid($sendgrid_username, $sendgrid_password, array("turn_off_ssl_verification" => true));
-//	$email = new SendGrid\Email();
-//	$email->addTo($to)->
-//	setFrom($from)->
-//	setSubject('Your account has been activated')->
-//	setText($emailContent)->
-//	setHtml($emailContent)->
-//	addHeader('X-Sent-Using', 'SendGrid-API')->
-//	addHeader('X-Transport', 'web');
-//	$response = $sendgrid->send($email);
+	$sendgrid_username = "internship-espresoh";
+	$sendgrid_password = "internship-project1";
+	$emailContent = "Hello there (: <br> Looks like your account have been aproved by the admin. Excited? <br> You can access the application now at http://esohintern.bucatzel.ro/login.php. <br> Enjoy and have a nice day!";
+	$to = $email[0]['google_email'];
+	$from = 'lex.greenapple@gmail.com';
+	$sendgrid = new SendGrid($sendgrid_username, $sendgrid_password, array("turn_off_ssl_verification" => true));
+	$email = new SendGrid\Email();
+	$email->addTo($to)->
+	setFrom($from)->
+	setSubject('Your account has been activated')->
+	setText($emailContent)->
+	setHtml($emailContent)->
+	addHeader('X-Sent-Using', 'SendGrid-API')->
+	addHeader('X-Transport', 'web');
+	$response = $sendgrid->send($email);
 	$successMessage = "Role assigned!";
 	$_SESSION['successMessage'] = $successMessage;
 	header('Location: ' . $GLOBALS['path'] . 'admin/pending-requests.php');
