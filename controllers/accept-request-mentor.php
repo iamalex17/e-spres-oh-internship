826914cff64cdd90c1b195ab797bc589cfcf4d6d
iamalex17 <ungureanualex17@yahoo.com>
2015-10-22 11:25:28 +0300

    Changed the text the user receives in the email

--- controllers/accept-request-mentor.php
+++ controllers/accept-request-mentor.php
@@ -23,7 +23,7 @@ if($status == 1) {
 	$email = ConnectToDB::interogateDB($sql, $valuesToBind);
 	$sendgrid_username = "internship-espresoh";
 	$sendgrid_password = "internship-project1";
-	$emailContent = "Hello there (: <br> Looks like your account have been aproved by the admin. Excited? <br> You can access the application now at http://esohintern.bucatzel.ro/login.php. <br> Enjoy and have a nice day!";
+	$emailContent = "<h3>Hello there (:</h3> <p>Welcome! Looks like your account have been aproved by the admin. Excited? Well I hope so, you can enjoy all the mythy powers you have been granted as a mentor. Use them wisely! (:</p> <p>You can access the application now at http://esohintern.bucatzel.ro/login.php.</p> <h4>Enjoy and have a nice day!</h4>";
 	$to = $email[0]['google_email'];
 	$from = 'lex.greenapple@gmail.com';
 	$sendgrid = new SendGrid($sendgrid_username, $sendgrid_password, array("turn_off_ssl_verification" => true));
