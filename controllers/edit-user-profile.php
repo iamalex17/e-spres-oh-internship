f666040efd405d37e7c36e193bb46141c8a1eeea
iamalex17 <ungureanualex17@yahoo.com>
2015-10-26 13:46:30 +0200

    Added functionality to change google users image when editing profile

--- controllers/edit-user-profile.php
+++ controllers/edit-user-profile.php
@@ -5,11 +5,6 @@ require_once '../classes/class.user.php';
 
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
 	session_start();
-	$request = User::verifyRequestURL($_SERVER['HTTP_REFERER']);
-	if($request != 'edit-profile.php') {
-		header('Location: ' . $GLOBALS['path'] . 'dashboard.php');
-		exit();
-	}
 	$errorMessage = '';
 	$status = 1;
 	if((!isset($_POST['first_name'])) || (trim($_POST['first_name']) == '')) {
@@ -28,6 +23,27 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 			$sql = 'UPDATE `users` SET first_name = :first_name, last_name = :last_name WHERE google_id = :google_id';
 			$valuesToBind = array('last_name' => $lastName, 'first_name' => $firstName, 'google_id' => $_SESSION['google_id']);
 			ConnectToDB::interogateDB($sql, $valuesToBind);
+			if($_FILES['profile_image']['name'] != '') {
+				$sql = 'SELECT profile_image FROM `users` WHERE google_id = :google_id';
+				$valuesToBind = array('google_id' => $_SESSION['google_id']);
+				$result = ConnectToDB::interogateDB($sql, $valuesToBind);
+				$imagePath = "../images/user-profile-images/".$result[0]['profile_image'];
+				unlink($imagePath);
+				$fileName = $_FILES["profile_image"]["name"];
+				$fileTmp = $_FILES["profile_image"]["tmp_name"];
+				$ext = pathinfo($fileName,PATHINFO_EXTENSION);
+				$fileEncrypted = MD5($fileName) . '.' . $ext;
+				if(!file_exists("../images/user-profile-images/".$fileName)) {
+				move_uploaded_file($fileTmp=$_FILES["profile_image"]["tmp_name"],"../images/user-profile-images/".$fileEncrypted);
+				} else {
+					$fileName = basename($fileName,$ext);
+					$newFileName = MD5($fileName.time()).".".$ext;
+					move_uploaded_file($fileTmp=$_FILES["profile_image"]["tmp_name"],"../images/user-profile-images/".$newFileName);
+				}
+				$sql = 'UPDATE `users` SET profile_image = :profile_image WHERE google_id = :google_id';
+				$valuesToBind = array('profile_image' => $fileEncrypted, 'google_id' => $_SESSION['google_id']);
+				ConnectToDB::interogateDB($sql, $valuesToBind);
+			}
 			$successMessage = 'Your data has been successfully modified.';
 			$_SESSION['successMessage'] = $successMessage;
 			header('Location: ' . $GLOBALS['path'] . 'dashboard.php');
