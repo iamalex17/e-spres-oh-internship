f662a6bb877315e6dcc99b1e94f62f5335c376c0
iamalex17 <ungureanualex17@yahoo.com>
2015-11-11 14:30:54 +0200

    Fixed delete exercise when logged in with Google

--- controllers/delete-exercise.php
+++ controllers/delete-exercise.php
@@ -4,16 +4,13 @@ require_once '../controllers/load-template.php';
 require_once '../classes/class.connect-to-db.php';
 require_once '../classes/class.user.php';
 
+session_start();
+
+$successMessage = '';
+
 $request = explode('=', $_SERVER['HTTP_REFERER']);
 $request = end($request);
 $deleteExercise = $_POST['delete_course'];
-
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
-	if(!User::verifySessionID()) {
-		header('Location: ' . $GLOBALS['path'] . 'login');
-		exit();
-	}
-}
 
 $sql = 'UPDATE `exercises` SET status = 0 WHERE id = :id';
 $valuesToBind = array('id' => $deleteExercise);
@@ -23,6 +20,7 @@ $sql = 'UPDATE `submitted_exercises` SET status = 0 WHERE exercise_id = :exercis
 $valuesToBind = array('exerciseID' => $deleteExercise);
 ConnectToDB::interogateDB($sql, $valuesToBind);
 
-$_SESSION['successMessage'] = 'Exercise was succesfully deleted.';
+$successMessage = 'Exercise was succesfully deleted.';
+$_SESSION['successMessage'] = $successMessage;
 header('Location: ' . $GLOBALS['path'] . 'users/create-course?course_id=' . $request);
 ?>
