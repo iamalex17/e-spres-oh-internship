4fc2848d28e26d9aa8d51c78c1ed16a3051f2dc2
kristineVarga <v.kristine.c@gmail.com>
2015-11-17 14:43:32 +0200

    Increased height of wysiwyg on Create Course template

--- js/tab.js
+++ js/tab.js
@@ -37,6 +37,21 @@ var INTERACTION = (function() {
 		});
 	}
 
+	function basicInitTinyMCE() {
+		tinymce.init({
+			selector: "textarea",
+			mode: "textareas",
+			theme_advanced_path: true,
+			content_css : "../css/custom-content.css",
+			plugins: [
+				"advlist autolink lists link charmap print preview anchor",
+				"searchreplace visualblocks code fullscreen",
+				"insertdatetime media table contextmenu paste"
+			],
+			toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+		});
+	}
+
 	function changeOpenButtonText() {
 		if ($(this).is(":visible")) {
 			$(this).prev().find('.buttonOpen').text('Close');
